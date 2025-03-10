0eda26c574cbe9d36674f1655308f4968be4d540
kuropAqn <kuropan0819@gmail.com>
2025-03-10 07:05:53 +0000

    非同期通信　バグあり

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,8 @@
 class BookCommentsController < ApplicationController
-  before_action :is_matching_login_user, only: [:destroy]
 
   def create
     book = Book.find(params[:book_id])
+    
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
@@ -23,5 +23,6 @@ class BookCommentsController < ApplicationController
       redirect_to books_path
     end
 end
+
 
 end
